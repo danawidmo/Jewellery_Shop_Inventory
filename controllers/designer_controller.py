e8aae29a27d221399be4903787ed2f139d90bd43
Dana Varahi <dana_varahi@protonmail.com>
2020-11-09 13:39:39 +0000

    Added designers in the menu and a route to it

--- controllers/designer_controller.py
+++ controllers/designer_controller.py
@@ -1,7 +1,17 @@
 from flask import render_template, request, Blueprint
+import repositories.designer_repository as designer_repository
+
+designer_blueprint = Blueprint('designers', __name__)
 
 
-designer_blueprint = Blueprint('designer', __name__)
+# INDEX
+# Note the blueprint name is designer, the route is designers!
+@designer_blueprint.route("/designers")
+def designers():
+    designers = designer_repository.select_all()
+    return render_template('designers/index.html',designers=designers)
+
+
 
 
 # line 10 in index.html <a href='designers/{{designer.id}}'>
