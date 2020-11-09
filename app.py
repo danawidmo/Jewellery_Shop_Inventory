528287a6ad4af3da9766e4ab038caa6c4f5918f8
Dana Varahi <dana_varahi@protonmail.com>
2020-11-09 10:24:26 +0000

    Created Base and index html files and got them displaying in the browser

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
-from controllers.designer_controller import designer_blueprint
-from controllers.product_controller import product_blueprint
+# from controllers.designer_controller import designer_blueprint
+# from controllers.product_controller import product_blueprint
 
 
 app = Flask(__name__)
 
-app.register_blueprint(designer_blueprint)
-app.register_blueprint(product_blueprint)
+# app.register_blueprint(designer_blueprint)
+# app.register_blueprint(product_blueprint)
 
 
 @app.route('/')
