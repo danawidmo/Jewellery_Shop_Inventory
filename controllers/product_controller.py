4f4b3ef2e668ba967c436b8231bd6c2108d0c7cf
Dana Varahi <dana_varahi@protonmail.com>
2020-11-09 19:18:36 +0000

    Created route and template to add new products

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, redirect
 from models.product import Product
 import repositories.product_repository as product_repository
+import repositories.designer_repository as designer_repository
 
 product_blueprint = Blueprint('product', __name__)
 
@@ -13,3 +14,25 @@ product_blueprint = Blueprint('product', __name__)
 def product(id):
     product = product_repository.select(id)
     return render_template('/products/show.html', product=product)
+
+# CREATE
+# GET
+@product_blueprint.route("/new")
+def new_product():
+    designers = designer_repository.select_all()
+    return render_template("/products/new.html", designers=designers)
+
+# CREATE 
+# POST
+@product_blueprint.route("/new", methods = ['POST'])
+def create_product():
+    product_name = request.form['product_name']
+    description =request.form['description']
+    quantity = request.form['quantity']
+    cost = request.form['cost']
+    price = request.form['price']
+    designer_id = request.form['designer']
+    designer = designer_repository.select(designer_id)
+    product = Product(product_name, description, quantity, cost, price, designer)
+    product_repository.save(product)
+    return redirect('/')
