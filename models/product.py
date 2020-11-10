38f1550a8fd274b2b61f00497a4f1d17541b1a98
Dana Varahi <dana_varahi@protonmail.com>
2020-11-10 11:01:40 +0000

    Added low stock method to product class and passed correspondign tests

--- models/product.py
+++ models/product.py
@@ -10,3 +10,11 @@ class Product:
         self.cost = cost
         self.price = price
         self.designer = designer
+
+
+    def low_stock(self):
+        low_quantity= 5
+        if self.quantity <= low_quantity and self.quantity > 0:
+            return True
+        else:
+            return False
--- tests/test_product.py
+++ tests/test_product.py
@@ -7,6 +7,8 @@ class TestProduct(unittest.TestCase):
 
         self.designer = Designer('Dana', 'dana@gmail.com', 1)
         self.product = Product('Gold Ring', 'Golden ring with garnet stone', 10, 100, 200, self.designer, 3)
+        self.product2 = Product('Silver Ring', 'Silver ring with garnet stone', 4, 100, 200, self.designer, 3)
+        self.product3 = Product('Bronze Ring', 'Bronze ring with garnet stone', 0, 100, 200, self.designer, 3)
 
     def test_product_has_product_name(self):
         self.assertEqual('Gold Ring', self.product.product_name)
@@ -28,3 +30,9 @@ class TestProduct(unittest.TestCase):
 
     def test_product_has_id(self):
         self.assertEqual(3, self.product.id)
+
+    def test_low_stock(self):
+        self.assertEqual(True, self.product2.low_stock())
+
+    def test_low_stock_is_0(self):
+        self.assertEqual(False, self.product3.low_stock())
